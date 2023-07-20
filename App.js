d9f2e7ba2e0cf79f54b9f10c8047644e2d333a94
Adrian Pazinato <82603081+Adrian-Pazinato@users.noreply.github.com>
2023-07-20 17:04:33 -0300

    asd

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+export ANDROID_HOME = /Users/ < username > /Library/Android / sdk /
+export PATH = $PATH: $ANDROID_HOME / tools: $ANDROID_HOME / platform - tools
+
 export default function App() {
   return (
     <View style={styles.container}>
